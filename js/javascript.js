729396da06c11e599ae14e29c669b5447594f35b
Ne0Lux-C1Ph3r <neolux-cipher@hotmail.fr>
2019-10-23 14:45:31 +0200

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,52 @@
+/* 
+
+By Ne0Lux-C1Ph3r 
+http://Ne0Lux-C1Ph3r.github.io
+
+*/
+
+/* Function open 2 links */
+
+function openWindow(link1, link2) {
+    window.open(link1);
+    window.open(link2);
+};
+
+/* Functions Viewer in stripViewer */
+
+$(document).ready(function(){
+	
+	$(".home, .portfolio, .news, .write-up, .about").hide();
+
+	$("#home").click(function(){
+		$(".portfolio, .news, .write-up, .about").hide();
+		$(".home").slideDown();
+	});
+
+	$("#news").click(function(){
+		$(".home, .portfolio, .write-up, .about").hide();
+		$(".news").slideDown();
+	});
+
+	$("#write-up").click(function(){
+		$(".home, .portfolio, .news, .about").hide();
+		$(".write-up").slideDown();
+	});
+
+	$("#about").click(function(){
+		$(".home, .portfolio, .news, .write-up").hide();
+		$(".about").slideDown();
+	});
+
+/* Function color change links */
+
+	$("#link, #link1, #link2, #link3, #link4, #link5").hover(function(){
+		$(this).css("color", "green");
+	});
+	
+	$("#link, #link1, #link2, #link3, #link4, #link5").visited(function(){
+		$(this).stop().css("color", "white");
+	});
+	
+});
+
